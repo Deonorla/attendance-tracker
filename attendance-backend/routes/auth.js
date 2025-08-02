c35b04ab76804ed30b70eaad9567c5347bc5f177
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2025-08-02 09:46:20 +0100

    [auth config]

---
+++ attendance-backend/routes/auth.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+
+// Register a new staff
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists)
+    return res.status(400).json({ message: "User already exists" });
+
+  const user = await User.create({ name, email, password });
+  res
+    .status(201)
+    .json({
+      token: generateToken(user._id),
+      name: user.name,
+      email: user.email,
+    });
+});
+
+// Login staff
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || !(await user.matchPassword(password))) {
+    return res.status(401).json({ message: "Invalid credential" });
+  }
+});
+
+module.exports = router;
